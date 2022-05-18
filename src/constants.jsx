14e872dee1242440a31d3bd0eb350b923b871003
Vamsi Krishna. Aitha <ivamsi2001@gmail.com>
2022-05-18 14:37:28 +0530

    Emp & Dept Impl

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,9 +1,4 @@
 
-export const FILE_TYPES = Object.freeze({
-    IMAGE: Object.freeze(["JPEG", "PNG"]),
-    VIDEO: Object.freeze(["MP4", "M4V", "MOV", "3GP", "3G2", "WMV", "ASF", "AVI", "FLV", "MKV", "WEBM"])
-});
-
 export const RESPONSIVE = Object.freeze({
     HIDE_SM: Object.freeze({ display: { xs: "none", sm: "block" } }),
     SHOW_SM: Object.freeze({ display: { xs: "block", sm: "none" } })
